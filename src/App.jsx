940d884eed2304c968acb9934c80d6bfa1aa090f
georgiabains <georgiabains@hotmail.co.uk>
2024-09-19 00:23:15 +0100

    Created react player component and rendered Entity data

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,10 @@
-import { useState } from 'react'
-import Test from './components/test'
+import Player from "./components/player"
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <h1>Dungeon Crawler</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
-      <Test />
+      <Player />
     </>
   )
 }
---
+++ src/components/player.jsx
@@ -0,0 +1,16 @@
+import * as wasm from '../../wasm/pkg'
+
+export default function Player() {
+  const player = wasm.Entity.new(30, 30, 5, 'Ares')
+  return (
+    <>
+      <h2>Player</h2>
+      <ul>
+        <li>Name: {player.get_name()}</li>
+        <li>Current Health: {player.get_current_hp()}</li>
+        <li>Max Health: {player.get_total_hp()}</li>
+        <li>Attack: {player.get_attack()}</li>
+      </ul>
+    </>
+  )
+}
