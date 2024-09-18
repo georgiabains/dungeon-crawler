46d77978d2a8a6cfbc7c6d0c8bdcb0cb8b3cca8a
georgiabains <georgiabains@hotmail.co.uk>
2024-09-19 00:14:28 +0100

    Pasted my wasm-combat-demo Rust code and initialised a test Player entity

--- src/components/test.jsx
+++ src/components/test.jsx
@@ -1,6 +1,8 @@
 import * as wasm from '../../wasm/pkg'
 
 export default function Test() {
+  const player = wasm.Entity.new(30, 30, 5, 'player')
+  console.log(player)
   return (
     <>
       <p>{wasm.greet('test')}</p>
