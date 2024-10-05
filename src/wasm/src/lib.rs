92c672b65de7273fc1c176a6a88f8428982bec7c
georgiabains <georgiabains@hotmail.co.uk>
2024-10-05 21:19:34 +0100

    Added components and simplified all entities into one entities vec

--- src/wasm/src/lib.rs
+++ src/wasm/src/lib.rs
@@ -12,6 +12,7 @@ type EntityIndex = usize;
  * Components.
  * - Add in alphabetical order.
  */
+struct Action(&'static str);
 struct Attack(u64);
 struct Attributes {
     dexterity: usize,
@@ -23,24 +24,32 @@ struct CombatCoordinate {
     y: u16,
 }
 struct Defence(u64);
+struct Difficulty(u16); // My decision on what enemies are harder than others; apply to each entitity differently based on Class
+struct DifficultyModifier(f16); // Player-selected; 0 is easiest
 struct GearSlot(&'static str); // TODO: Figure out how to store slot. Needs to allow different positions on the body.
 struct Health {
     current: f32,
     maximum: f32,
     temporary: f32,
 }
-struct Inventory(Vec<EntityIndex>); // TODO: Needs to be list of Item Entities only (or references to Item Entities)
+// Faction? IsLeader? HasLeader?
+struct Equipped(Vec<EntityIndex>);
+struct Inventory(Vec<EntityIndex>);
 struct IsConsumable(bool);
 struct IsEnemy(bool);
 struct IsEquippable(bool);
-struct IsEquipped(bool);
 struct IsPlaceable(bool);
 struct IsPlayer(bool);
+struct IsTargetable(bool);
+struct ItemsOnDeath(Vec<EntityIndex>);
+struct Level(u32);
 struct Mana(u64);
 struct Name(&'static str);
 struct Personality(&'static str); // TODO: Maybe splitting into separate bools makes more sense, but I'll do this for now.
-struct Skill(&'static str); // TODO: This doesn't feel right. Should there by an Entity called Input? IDK.
 struct Thumbnail(bool); // TODO: Figure out image storage.
+struct Value(u64); // Gold
+struct Weight(u64);
+struct Xp(u32);
 
 /**
  * Systems.
@@ -57,12 +66,13 @@ struct GameState {
     /**
      * Entities.
      */
-    characters: Vec<EntityIndex>,
-    items: Vec<EntityIndex>,
+    entities: Vec<EntityIndex>,
 
     /**
      * Components.
      */
+    // All of these component vecs must be the same length, which is the current
+    // number of entities.
     attack_components: Vec<Option<Attack>>,
     attributes_components: Vec<Option<Attributes>>,
     class_components: Vec<Option<Class>>,
@@ -82,4 +92,8 @@ struct GameState {
     personality_components: Vec<Option<Personality>>,
     skill_components: Vec<Option<Skill>>,
     thumbnail_components: Vec<Option<Thumbnail>>,
+
+    // char 0
+    // attack_components[0] = attack value for character at characters[0]
+
 }
