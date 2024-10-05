c721cc324b5b96ac9c87aa0f0e571d3c0c3c6b17
georgiabains <georgiabains@hotmail.co.uk>
2024-10-05 22:43:14 +0100

    Create new game state with all component arrays

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import Test from '~/components/test'
+
 function App() {
   return (
     <>
       <h1>Dungeon Crawler</h1>
+      <Test />
     </>
   )
 }
--- src/components/test.jsx
+++ src/components/test.jsx
@@ -1,8 +1,14 @@
 import * as wasm from '~/wasm/pkg'
 
 export default function Test() {
-  const player = wasm.Entity.new(30, 30, 5, 'player')
-  console.log(player)
+  // console.log(wasm)
+  const gameState = wasm.GameState.new()
+  console.log(gameState)
+  console.log(gameState.add_entity())
+  console.log(gameState.add_name_to_entity(0, "test"))
+  console.log(gameState.add_entity())
+  console.log(gameState.add_name_to_entity(1, "goblin"))
+  console.log(gameState.get_entities())
   return (
     <>
       <p>{wasm.greet('test')}</p>
