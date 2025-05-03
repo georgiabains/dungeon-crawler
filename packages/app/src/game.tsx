6ec914195a51eea46a22d0c393befb8c8412725f
georgiabains <georgiabains@hotmail.co.uk>
2025-05-03 17:23:36 +0100

    Add dynamic input for player name

--- packages/app/src/game.tsx
+++ packages/app/src/game.tsx
@@ -1,13 +1,21 @@
+import { useState } from 'react'
 import { sum_test } from '../../library/dist'
 
-function Game() {
+type Entity  = {
+  name: string
+}
 
+function Game() {
+  // Test that confirms running WASM for calculations is possible
   console.log(sum_test(2, 2))
+  const [player, setPlayer] = useState({} as Entity)
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const form = event.currentTarget;
     const formData = new FormData(form);
+
+    setPlayer({ name: formData.get('entity') as string })
   }
 
   return (
@@ -17,6 +25,8 @@ function Game() {
         <input id="CreateEntity" name="entity" />
         <button type="submit">Submit</button>
       </form>
+
+      <p>{player.name}</p>
     </>
   )
 }
