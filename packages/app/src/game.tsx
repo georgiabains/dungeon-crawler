ce73922a6c234c0618a0c97f261cde6de704d36e
georgiabains <georgiabains@hotmail.co.uk>
2025-05-03 18:46:06 +0100

    Set up choice of 3 encounters, along with a screen showing player choice (name only)

--- packages/app/src/game.tsx
+++ packages/app/src/game.tsx
@@ -1,6 +1,11 @@
 import { useEffect, useState } from 'react'
 import { sum_test } from '@workspace/library'
 
+type Encounter = {
+  name: string,
+  type: string
+}
+
 type Entity = {
   isLive: boolean,
   name: string,
@@ -39,31 +44,54 @@ function Game() {
   // console.log(sum_test(2, 2))
   const [player, setPlayer] = useState(() => load('player'))
   const [showDungeon, setShowDungeon] = useState(false)
+  const [encounter, setEncounter] = useState({} as Encounter)
 
   useEffect(() => {
     window.sessionStorage.setItem('player', JSON.stringify(player))
-  }, [player])
+  }, [player as Entity])
 
-  const handleCreateCharacter = (event: React.FormEvent<HTMLFormElement>) => {
+  const handleCreateCharacter = ((event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    const form = event.currentTarget;
-    const formData = new FormData(form);
+    const formData = new FormData(event.currentTarget)
 
     setPlayer({ 
       isLive: true, 
       name: formData.get('entity') as string,
       weapon: selectWeapon(formData.get('weapon' as string) as string)
     })
-  }
+  })
 
-  const selectWeapon = (weaponName: string) => {
+  const selectWeapon = ((weaponName: string) => {
     return weapons[weaponName as keyof typeof weapons]
-  }
+  })
 
   const handleEnterDungeon = (() => setShowDungeon(true))
 
-  const handleChooseEncounter = (() => {
-    // todo
+  const handleChooseEncounter = ((event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget)
+
+    setEncounter({
+      name: setEncounterName(formData.get('encounter' as string) as string),
+      type: formData.get('encounter' as string) as string
+    })
+  })
+
+  const setEncounterName = ((encounterType: string) => {
+    let name: string = ''
+
+    switch (encounterType) {
+      case 'loot':
+        name = 'Chest of Unknown Riches'
+        break
+      case 'combat':
+        name = 'Combat vs Goblins'
+        break
+      case 'rest':
+        name = 'Recuperate at Camp'
+    }
+
+    return name
   })
 
   return (
@@ -113,27 +141,34 @@ function Game() {
         showDungeon
           ? <>
               <h2>Dungeon</h2>
-              <form method="post" onSubmit={handleChooseEncounter}>
-                <fieldset>
-                  <legend>Choose Encounter</legend>
 
-                  {/* Idea will be that encounters are part of a "generation" or randomisation function, and 3 will be chosen based on probability (i.e. higher chance to get combat or rest over loot or a special event) */}
-                  <div>
-                    <input id="EncounterLoot" name="encounter" value="loot" type="radio" />
-                    <label htmlFor="EncounterLoot">Loot chest</label>
-                  </div>
+              {
+                encounter.name 
+                ? <p>{encounter.name}</p>
+                : <form method="post" onSubmit={handleChooseEncounter}>
+                    <fieldset>
+                      <legend>Choose Encounter</legend>
 
-                  <div>
-                    <input id="EncounterCombat" name="encounter" value="combat" type="radio" />
-                    <label htmlFor="EncounterCombat">Combat</label>
-                  </div>
+                      {/* Idea will be that encounters are part of a "generation" or randomisation function, and 3 will be chosen based on probability (i.e. higher chance to get combat or rest over loot or a special event) */}
+                      <div>
+                        <input id="EncounterLoot" name="encounter" value="loot" type="radio" />
+                        <label htmlFor="EncounterLoot">Loot chest</label>
+                      </div>
 
-                  <div>
-                    <input id="EncounterRest" name="encounter" value="rest" type="radio" />
-                    <label htmlFor="EncounterRest">Rest</label>
-                  </div>
-                </fieldset>
-              </form>
+                      <div>
+                        <input id="EncounterCombat" name="encounter" value="combat" type="radio" />
+                        <label htmlFor="EncounterCombat">Combat</label>
+                      </div>
+
+                      <div>
+                        <input id="EncounterRest" name="encounter" value="rest" type="radio" />
+                        <label htmlFor="EncounterRest">Rest</label>
+                      </div>
+                    </fieldset>
+
+                    <button type="submit">Select</button>
+                  </form>
+              }
             </>
           : ''
       }
