9d8610da1c423004c3172fd2eb85610b17419aed
georgiabains <georgiabains@hotmail.co.uk>
2025-10-17 11:21:33 +0100

    Build symbols util file to keep component names unique

---
+++ packages/app/src/utils/symbols.ts
@@ -0,0 +1,11 @@
+/**
+ * Utils: Symbols
+ * - Reference file for all symbols used in the game.
+ * - Prevent duplicate names.
+ */
+const Symbols = {
+  attack: Symbol("Attack"),
+  health: Symbol("Health")
+}
+
+export default Symbols
