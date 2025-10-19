8f8c19132be69e0058af3c17290e08e12b2a97e7
georgiabains <georgiabains@hotmail.co.uk>
2025-10-19 20:57:50 +0100

    Remove test system

--- packages/app/src/game/test.ts
+++
@@ -1,8 +0,0 @@
-import Symbols from "../utils/symbols"
-import { Systems } from "../engine/engine"
-
-
-export const test = Systems.createSystem([Symbols.attack, Symbols.health], (world, requiredComponents) => {
-  console.log(requiredComponents)
-  return world;
-})
