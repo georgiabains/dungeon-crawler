7a9f3416571dc1e62bc87fc9a46f9f022c7823cb
georgiabains <georgiabains@hotmail.co.uk>
2025-11-11 19:20:45 +0000

    Remove unused types and add comments

--- packages/app/src/types.ts
+++ packages/app/src/types.ts
@@ -1,7 +1,13 @@
 import { JSX, ReactElement } from 'react'
 
+/**
+ * Entity.
+ */
 export type Entity = string
 
+/**
+ * Component.
+ */
 export type ComponentName = string
 export type ComponentData = Map<Entity, unknown> // unknown because a component can be anything
 
@@ -10,12 +16,18 @@ export type Component = {
   data: ComponentData
 }
 
+/**
+ * Game World.
+ */
 export type GameWorld = {
   entities: Array<Entity>,
   components: Map<Component["name"], Component["data"]>,
   newEntity: Entity
 }
 
+/**
+ * Game Store.
+ */
 export type GameStoreComponent = {
   name: ComponentName,
   data: any,
@@ -29,21 +41,19 @@ export type GameStore = {
   getComponent: (component: string) => any,
 }
 
+/**
+ * System.
+ */
 export type System = (world: GameWorld, params?: any) => GameWorld
+
 export type SystemPayload = {
   system: System,
   params?: any
 }
-export type SystemForEntities = {
-  run: System,
-  entities?: Array<Entity>
-}
 
-export type Action = {
-  healthValue?: number,
-  type: string
-}
-
+/**
+ * Other.
+ */
 export type Encounter = {
   render: JSX.Element | undefined,
   name: string,
@@ -55,24 +65,9 @@ export type EncounterParam = {
   type: string
 }
 
-export type EntityOld = {
-  health?: number,
-  id: number,
-  isLive?: boolean,
-  isParty?: boolean,
-  isSelectable?: boolean,
-  name: string,
-  weapon?: Weapon,
-  agility?: number | undefined
-}
-
 export type PropsConditionalText = {
   isShow: boolean,
   element: ReactElement
-}
-
-export type PropsPlayer = {
-  player: EntityOld
 }
 
 export type PropsScreenInitialisation = {
