7d2237b0b3faaa8fc5884b93a54be46d405757aa
georgiabains <georgiabains@hotmail.co.uk>
2025-11-13 18:11:32 +0000

    Add comments to screen game menu

--- packages/app/src/game/screen-game-menu.tsx
+++ packages/app/src/game/screen-game-menu.tsx
@@ -1,6 +1,22 @@
+/**
+ * Screen: Game Menu
+ * 
+ * Render the game menu.
+ */
+import { ReactElement } from 'react'
 import { PropsScreenGameMenu } from '../types'
 
-function ScreenGameMenu({setGame}: PropsScreenGameMenu) {
+/**
+ * Render game menu.
+ * @param {Function} data.setGame - Callback function to set game info. 
+ * @returns {ReactElement}
+ */
+function ScreenGameMenu({ setGame }: PropsScreenGameMenu): ReactElement {
+  
+  /**
+   * Handle creating a new game.
+   * @param {React.FormEvent<HTMLFormElement>} event - Submit event.
+   */
   function handleNewGame(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault()
     setGame(crypto.randomUUID())
